5434195737f30f4d8e18f9039951d386ae3c9f00
Matt McElwee <mubatt@wyopub.com>
2017-08-09 02:40:36 -0500

    More form handling

--- public/contact.js
+++ public/contact.js
@@ -1,7 +1,17 @@
 $(() => {
 	$(".form-group input").focus(focusLabel);
+	$(".form-group input").focusout(removeFocus);
 });
 
 const focusLabel = (e) => {
 	$(e.target).parent().find("label").addClass("active focus");
 };
+
+const removeFocus = (e) => {
+	if ($(e.target).val().length) {
+		$(e.target).parent().find("label").removeClass("active");
+	} else {
+		$(e.target).parent().find("label").removeClass("active");
+		$(e.target).parent().find("label").removeClass("focus");
+	}
+}
