5b8e61f4f71f452d85130f7ddc8417a2e6f34862
Matt McElwee <mubatt@wyopub.com>
2017-08-09 00:14:50 -0500

    Made major SEO improvements, adding Twitter Card, OG, and Meta Data. Worked more on Portfolio templating (should work?)

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,5 +1,27 @@
 $(() => {
-	$.getJSON("work.json", (json) => {
-		console.log(json);
+	$.getJSON("/public/work.json", (json) => {
+		for (var i = 0; i < json.work.length; i++) {
+			let args = {
+				imageURI = json.work[i].image,
+				link = json.work[i].href,
+				role = json.work[i].role,
+				status = json.work[i].status.label,
+				statusColor = json.work[i].status.color,
+				title = json.work[i].title,
+				description = json.work[i].description,
+				projectYear = json.work[i].year
+			};
+
+			let html = $("#portfolio").html().trim();
+			for (var arg in args) {
+				html = replaceAll(html, `{{ ${arg} }}`, args[arg]);
+			}
+			$(".portfolio-container").append($(html));
+		}
 	});
 });
+
+
+const replaceAll = (string, sub, replace) => {
+	return string.split(sub).join(replace);
+};
