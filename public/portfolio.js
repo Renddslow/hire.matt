8b8b8ba42b33a71a4d51a3bb1ef45d94323c1931
Matt McElwee <mubatt@wyopub.com>
2017-08-09 00:16:19 -0500

    Fixed an issue with args in portfolio.js

--- public/portfolio.js
+++ public/portfolio.js
@@ -2,14 +2,14 @@ $(() => {
 	$.getJSON("/public/work.json", (json) => {
 		for (var i = 0; i < json.work.length; i++) {
 			let args = {
-				imageURI = json.work[i].image,
-				link = json.work[i].href,
-				role = json.work[i].role,
-				status = json.work[i].status.label,
-				statusColor = json.work[i].status.color,
-				title = json.work[i].title,
-				description = json.work[i].description,
-				projectYear = json.work[i].year
+				imageURI: json.work[i].image,
+				link: json.work[i].href,
+				role: json.work[i].role,
+				status: json.work[i].status.label,
+				statusColor: json.work[i].status.color,
+				title: json.work[i].title,
+				description: json.work[i].description,
+				projectYear: json.work[i].year
 			};
 
 			let html = $("#portfolio").html().trim();
