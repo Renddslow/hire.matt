f5e8107c85a2b154bd8fa8af4bd7506a4a582e13
Matt McElwee <mubatt@wyopub.com>
2017-08-09 02:57:26 -0500

    Form handling, submission, error, and success displays

--- public/contact.js
+++ public/contact.js
@@ -3,6 +3,17 @@ $(() => {
 	$(".form-group input").focusout(removeFocus);
 	$(".form-group textarea").focus(focusLabel);
 	$(".form-group textarea").focusout(removeFocus);
+
+	$("form").submit((e) => {
+		e.preventDefault();
+		var data = {
+			name: $("#name").val().trim(),
+			email: $("#email").val().trim(),
+			message: $("#description").val().trim()
+		};
+		handleForm(data, displaySuccess, displayError);
+		return false;
+	})
 });
 
 const focusLabel = (e) => {
@@ -16,4 +27,29 @@ const removeFocus = (e) => {
 		$(e.target).parent().find("label").removeClass("active");
 		$(e.target).parent().find("label").removeClass("focus");
 	}
+};
+
+const handleForm = (data, cb, err) => {
+	$.ajax({
+		type: 'POST',
+		data: data,
+		url: 'http://api.mattmcelwee.com/v1/contact',
+		success: (res) => {
+			displaySuccess();
+		},
+		error: (res) => {
+			console.log(res);
+			displayError();
+		}
+	});
+};
+
+const displaySuccess = () => {
+	$("form").remove();
+	$(".contact").append($("<p>Awesome! I'll take a look at your message as soon as I can. I'm so looking forward to talk with you more.</p>"));
+}
+
+const displayError = () => {
+	$("form").remove();
+	$(".contact").append($("<p><strong>Whoops!</strong></p><p>Looks like there was an issue submitting the form. Well I don't usually do this for just anyone, but since I like you, how `bout you just <a href=\"mailto:mubatt@wyopub.com\">send me an email.</a>"));
 }
